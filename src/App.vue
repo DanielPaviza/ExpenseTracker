f804e78595ead3d609611616478106290f61652b
Daniel Pavíza <daniel.paviza@gmail.com>
2025-11-16 10:20:07 +0100

    window size change, price summary, css colors

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { onMounted } from 'vue'
 import SpendingsDashboard from '@components/SpendingsDashboard/Dashboard.vue'
 import { useSpendingsStore } from '@stores/spendingsStore'
+import Header from '@components/Header.vue'
 
 import SpendingsList from '@components/spendingsList/SpendingsList.vue'
 import Loading from '@components/Loading.vue'
@@ -14,16 +15,14 @@ onMounted(async () => {
 </script>
 
 <template>
-  <header class="mb-8 text-center">
-    <h1 class="text-4xl font-extrabold tracking-tight text-blue-700">Evidence nákupů</h1>
-  </header>
-  <!-- Tailwind test element: should be red with white text and padding -->
-  <div class="bg-red-500 text-white p-4 mb-4 text-center rounded">
-    Tailwind test: This should be red with white text.
+  <div class="min-h-screen bg-white">
+    <div class="mx-8">
+      <Header />
+      <Loading v-if="spendingsStore.isLoading" />
+      <template v-else>
+        <SpendingsDashboard />
+        <SpendingsList />
+      </template>
+    </div>
   </div>
-  <Loading v-if="spendingsStore.isLoading" />
-  <template v-else>
-    <SpendingsDashboard />
-    <SpendingsList />
-  </template>
 </template>
