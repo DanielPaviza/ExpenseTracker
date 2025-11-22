9539d3b7e4ef91236243b3a3c7cc288d1630c197
Daniel Pavíza <daniel.paviza@gmail.com>
2025-11-22 20:29:29 +0100

    appContent component, pendingChanges refactor, error toasts

---
+++ src/components/AppContent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import Header from '@components/Header.vue'
+  import { useSpendingsStore } from '@stores/spendingsStore'
+  import { useMessage } from 'naive-ui'
+
+  import { onMounted } from 'vue'
+
+  const spendingsStore = useSpendingsStore()
+  const message = useMessage()
+
+  onMounted(async () => {
+    try {
+      await spendingsStore.load()
+    } catch (error) {
+      message.error('Chyba při načítání dat')
+      console.error('Load error:', error)
+    }
+  })
+</script>
+
+<template>
+  <Header />
+  <router-view />
+</template>
