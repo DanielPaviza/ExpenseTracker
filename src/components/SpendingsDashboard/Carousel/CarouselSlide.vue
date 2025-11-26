a14809e7072f7aefd4ca1067c4a4f14498592725
Daniel Pavíza <daniel.paviza@gmail.com>
2025-11-26 16:26:46 +0100

    disardChanges fix, test env header

--- src/components/SpendingsDashboard/Carousel/CarouselSlide.vue
+++ src/components/SpendingsDashboard/Carousel/CarouselSlide.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex flex-wrap gap-5 justify-between">
+  <div class="flex flex-wrap gap-5 justify-between px-1">
     <slot />
   </div>
 </template>
